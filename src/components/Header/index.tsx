b45d2c71a05a3f0ce89212168e1cd9a9ba5f550a
Itzac Angelo Albertin <itzacalbertin@gmail.com>
2022-02-13 17:14:14 -0300

    got posts from prismic and added conditional active classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import { SingInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
+import { ActiveLink } from '../ActiveLink';
+
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="Ignews" />
                 <nav>
-                    <a href="#" className={styles.active}>Home</a>
-                    <a href="#">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a >Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a >Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SingInButton />
